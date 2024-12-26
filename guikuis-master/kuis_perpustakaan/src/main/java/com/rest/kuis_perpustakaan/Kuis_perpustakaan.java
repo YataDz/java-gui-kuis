8138037a91ddb7aaf025959bca2674e87ba19192
Dimas Arya P.S <122659347+YataDz@users.noreply.github.com>
2024-12-26 20:45:25 +0700

    Add files via upload

---
+++ guikuis-master/kuis_perpustakaan/src/main/java/com/rest/kuis_perpustakaan/Kuis_perpustakaan.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.rest.kuis_perpustakaan;
+
+/**
+ *
+ * @author rsstst
+ */
+//test update 2
+public class Kuis_perpustakaan {
+
+    public static void main(String[] args) {
+       login awal = new login();
+       awal.setVisible(true);
+       awal.pack();
+       awal.setLocationRelativeTo(null);
+       awal.setDefaultCloseOperation(login.EXIT_ON_CLOSE);
+    }
+}
